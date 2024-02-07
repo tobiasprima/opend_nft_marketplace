93474a8b69b1b5c804856f06c5e8e89e0b8b45af
Tobias Prima <primatjahja@gmail.com>
2024-02-07 15:35:04 +0700

    fix: delete components

--- src/opend_assets/src/components/App.jsx
+++ src/opend_assets/src/components/App.jsx
@@ -1,21 +1,15 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import Minter from "./Minter";
-import Item from "./Item";
 import "bootstrap/dist/css/bootstrap.min.css";
-import homeImage from "../../assets/home-img.png";
 
 function App() {
 
   const NFTID = "rrkah-fqaaa-aaaaa-aaaaq-cai";
 
-  return (    
+  return (
     <div className="App">
       <Header />
-      {/* <Minter></Minter> */}
-      {/* <Item id={NFTID}></Item>  */}
-      <img className="bottom-space" src={homeImage} />
       <Footer />
     </div>
   );
