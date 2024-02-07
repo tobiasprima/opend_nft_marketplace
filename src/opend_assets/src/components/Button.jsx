5fac8471ae8309a4e176dc5fcb6c0413025e8edf
Tobias Prima <primatjahja@gmail.com>
2024-02-07 17:12:25 +0700

    feat: button components

---
+++ src/opend_assets/src/components/Button.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Button(){
+    return (
+        <div className="Chip-root makeStyles-chipBlue-108 Chip-clickable">
+            <span
+              onClick={}
+              className="form-Chip-label"
+            >
+              Sell
+            </span>
+        </div>
+    )
+}
+
+export default Button;
